c26a200978495102cbd149882a6733f85bb689b1
habioye <78454514+habioye@users.noreply.github.com>
2025-03-05 04:18:52 -0600

    added the speed benchmark for Python

--- Speed Test/sum_to_n.py
+++ Speed Test/sum_to_n.py
@@ -2,6 +2,7 @@
 
 # Python code
 import time
+import yaml
 
 def sum_python(n):
 	result = 0
@@ -14,3 +15,7 @@ start_time = time.time()
 sum_python(n)
 end_time = time.time()
 print(f"Python: Sum of 1 to {n} is {sum_python(n)} calculated in {end_time - start_time:.4f} seconds")
+with open("config.yaml") as f:
+	cfg = yaml.load(f, Loader= yaml.FullLoader)
+
+print(cfg)
