2dea788ce184b8acd39b1564bc99bae4bdb3e871
habioye <78454514+habioye@users.noreply.github.com>
2025-10-19 02:08:31 -0500

    Created a basic example of different type of constructors(including move constructors) and assignment operators

--- C++/MoveConstructor.cpp
+++ C++/MoveConstructor.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <print>
+#include <cstdio>
+#include "stdc++.h"
 
 using namespace std;
 class Simple {
@@ -11,23 +14,29 @@ public:
 		cout << "Constructor called, data = " << *data
 		<< endl;
 	}
-	
+
 	// Destructor
 	~Simple()
 	{
 		delete data;
 		cout << "Destructor called" << endl;
 	}
-	
+
+	// Copy Constructor
+	Simple(Simple& other) :data (other.data) {
+	std::cout << "Copy constructor called, data = " << this->data << std::endl;
+	}
+
 	// Move constructor
 	Simple(Simple&& other)
 	: data(other.data)
 	{
 		// nullify the other object resource
+		// This kind of hollows out the object.
 		other.data = nullptr;
 		cout << "Move constructor called from constructor" << endl;
 	}
-	
+
 	// Move assignment operator
 	Simple& operator=(Simple&& other)
 	{
@@ -39,11 +48,11 @@ public:
 		}
 		return *this;
 	}
-	
+
 	// Disable copy constructor and copy assignment operator
 	Simple(const Simple&) = delete;
 	Simple& operator=(const Simple&) = delete;
-	
+
 	// Function to print the value
 	void print()
 	{
@@ -54,27 +63,25 @@ public:
 			cout << "Data is null" << endl;
 		}
 	}
-	
+
 };
 
 int main(int argc, char *argv[]) {
 	// Create a Simple object with value 42
 	Simple obj1(42);
 	obj1.print();
-	
+
 	// Move obj1 to obj2 using move constructor
-	Simple obj2 = move(obj1);
+	Simple obj2 = std::move(obj1);
 	// Print obj2's data
 	obj2.print();
 	// Print obj1's data after move
 	obj1.print();
-	
+
 	// Create another Simple object with value 84
 	Simple obj3(84);
 	// Move obj2 to obj3 using move assignment
-	obj3 = move(obj2);
-	obj3.print();
-	obj2.print();
-	
+	obj3 = std::move(obj2);
+
 	return 0;
 }
