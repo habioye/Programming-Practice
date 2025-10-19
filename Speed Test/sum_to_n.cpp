75857a322d3a2d4a1d0da269e5dab1ca3f78a33e
habioye <78454514+habioye@users.noreply.github.com>
2025-10-19 02:42:56 -0500

    Performed Speed test to see the time difference between C++ and Python

--- Speed Test/sum_to_n.cpp
+++ Speed Test/sum_to_n.cpp
@@ -1,6 +1,9 @@
 // C++ code
 #include <iostream>
 #include <chrono>
+#include <typeinfo>
+#include "yaml-cpp/yaml.h"
+
 
 long long sum_cpp(int n) {
     long long result = 0;
@@ -12,10 +15,11 @@ long long sum_cpp(int n) {
 
 int main() {
     int n = 10000000;
-    auto start_time = std::chrono::high_resolution_clock::now();
+    std::chrono::time_point<std::chrono::high_resolution_clock> start_time = std::chrono::high_resolution_clock::now();
+//  std::cout << typeid(start_time).name() << std::endl;
     long long sum_result = sum_cpp(n);
-    auto end_time = std::chrono::high_resolution_clock::now(); // Wait for the user to respond.
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time);
+    std::chrono::time_point<std::chrono::high_resolution_clock> end_time = std::chrono::high_resolution_clock::now(); // Wait for the user to respond.
+    std::chrono::microseconds duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time);
     std::cout << "C++: Sum of 1 to " << n << " is " << sum_result << " calculated in " << duration.count() / 1000000.0 << " seconds" << std::endl;
     return 0;
 }
--- Speed Test/sum_to_n.py
+++ Speed Test/sum_to_n.py
@@ -14,8 +14,12 @@ n = 10000000
 start_time = time.time()
 sum_python(n)
 end_time = time.time()
-print(f"Python: Sum of 1 to {n} is {sum_python(n)} calculated in {end_time - start_time:.4f} seconds")
-with open("config.yaml") as f:
-	cfg = yaml.load(f, Loader= yaml.FullLoader)
-
-print(cfg)
+duration = start_time-end_time
+print(f"Python: Sum of 1 to {n} is {sum_python(n)} calculated in {duration:.4f} seconds")
+with open("Speed.yaml","r+") as f:
+	metric = yaml.load(f, Loader= yaml.FullLoader)
+	print(metric)
+	metric["Sum from 1 to 10000000 Speed(seconds)"]["Python"] = duration
+	
+	yaml.dump(metric,stream=f,default_flow_style=False, sort_keys=False)
+	print(metric)
